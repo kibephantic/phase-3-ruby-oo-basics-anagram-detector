cb06522ba215d56be5d4e95136d76403405c65d0
PatrickKibe <phantickibe01@gmail.com>
2022-08-21 10:25:54 +0300

    initial commit

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+
+    def initialize(word)
+        @word= word
+    end
+
+    def match(my_arr)
+       my_arr.select {|x| x.split("").sort == @word.split("").sort}
+    end
+
+end
